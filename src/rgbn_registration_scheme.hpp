81985303dec1a2fc4299868ad25d5ab60466a41a
Eundo Lee <cobed95@gmail.com>
2019-11-15 01:17:11 +0900

    add code from pcl example

--- src/rgbn_registration_scheme.hpp
+++ src/rgbn_registration_scheme.hpp
@@ -7,8 +7,34 @@
 #include "types.hpp"
 
 class RGBNRegistrationScheme: public TwoPhaseRegistrationScheme {
+typedef pcl::Normal normal;
+typedef pcl::PointCloud<normal> normal_point_cloud;
+typedef normal_point_cloud::Ptr normal_point_cloud_pointer;
+
 public:
     rgb_point_cloud_pointer extract_features(rgb_point_cloud_pointer cloud) {
+        normal_point_cloud_pointer ncloud_ptr(new normal_point_cloud);
+        pcl::IntegralImageNormalEstimation<rgb_point, normal> ne;
+
+        ne.setNormalEstimationMethod(ne.COVARIANCE_MATRIX);
+        ne.setNormalSmoothingSize(10.0f);
+        ne.setBorderPolicy(ne.BORDER_POLICY_MIRROR);
+        ne.setInputCloud(cloud);
+        ne.compute(*ncloud_ptr);
+
+        OrganizedEdgeFromRGBNormals<rgb_point, normal, pcl::Label> oed;
+        // Parameters below need some tweaking based on experiments.
+        oed.setInputNormals(ncloud_ptr);
+        oed.setInputCloud(cloud);
+        oed.setDepthDisconThreshold(10000);
+        oed.setMaxSearchNeighbors(50);
+
+        oed.setEdgeType(oed.EDGELABEL_RGB_CANNY);
+
+        pcl::PointCloud<pcl::Label> labels;
+        std::vector<pcl::PointIndices> label_indices;
+        oed.compute(labels, label_indices);
+
         return NULL;
     }
 
